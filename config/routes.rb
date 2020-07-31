f15f0169c00d0b11f1e419ae7d7e5ac7cd678113
Carlos Trotman <carlos_trotman@hotmail.com>
2020-07-31 14:15:27 +0800

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :cocktails do
+    resources :doses, only: [ :index, :new, :create ]
+  end
+  resources :doses, only: [ :show, :edit, :update, :destroy ]
+  end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,25 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+
+require "open-uri"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = JSON.parse(open(url).read)
+ingredients_arr = drinks.values[0]
+ingredients_arr.each_with_index do |e, index|
+ Ingredient.create!(name: ingredients_arr[index]["strIngredient1"])
+ puts "ingredient#{index} created!"
+end
+
+
+url2 = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+drinks = JSON.parse(open(url2).read)
+drinks_arr = drinks.values[0]
+drinks_arr.each_with_index do |e, index|
+ Cocktail.create!(name: drinks_arr[index]["strDrink"])
+ puts "cocktail#{index} created!"
+end
